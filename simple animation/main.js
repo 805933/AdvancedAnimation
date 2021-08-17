60e682307779ac1944c2c1b42af2eb6459b4734a
805933 <805933@seq.org>
2021-08-17 13:24:01 -0700

    two balls bouncing

--- simple animation/main.js
+++ simple animation/main.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", init);
 
 // global variables
-var canvas, context, x, y, dx, dy;
+var canvas, context, x, y, dx, dy, x2, y2, dx2, dy2;
 
 function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
@@ -13,6 +13,8 @@ function init(){
 
     x = y = 100;    // initial x,y canvas location
     dx = dy = 2;    // velocity in x and y directions
+    x2 = y2 = 150;
+    dx2 = dy2 = 5;
 
     animate();      // kick off the animation
 }
@@ -23,7 +25,8 @@ function animate() {
     context.clearRect(0,0,canvas.width,canvas.height);
     checkEdges();
     update();   // update location
-    draw();     // render
+    draw();    // render
+    draw2();
     requestAnimationFrame(animate); // next cycle
 }
 
@@ -31,6 +34,8 @@ function animate() {
 function update() {
     x += dx;    // update x coordinate of location with x velocity
     y += dy;    // update y coordinate of location with y velocity
+    x2 += dx2;
+    y2 += dy2;
 }
 
 function checkEdges() {
@@ -39,6 +44,12 @@ function checkEdges() {
   }
   if(y<0 || y>canvas.height){
     dy *= -1
+  }
+  if(x2<0 || x2>canvas.width){
+    dx2 *= -1
+  }
+  if(y2<0 || y2>canvas.height){
+    dy2 *= -1
   }
 }
 
@@ -51,6 +62,18 @@ function draw() {
     context.arc(x, y, radius, 0, 2 * Math.PI);
     context.strokeStyle = "black";  // color to fill
     context.fillStyle = "blue";     // color to stroke
+    context.fill();     // render the fill
+    context.stroke();   // render the stroke
+}
+
+function draw2() {
+    let radius = 20; // local variable radius of the circle
+    // create the circle path
+    context.beginPath();    // clear old path
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+    context.arc(x2, y2, radius, 0, 2 * Math.PI);
+    context.strokeStyle = "yellow";  // color to fill
+    context.fillStyle = "purple";     // color to stroke
     context.fill();     // render the fill
     context.stroke();   // render the stroke
 }
