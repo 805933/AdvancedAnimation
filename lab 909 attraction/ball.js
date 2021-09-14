9f65d846d0b23a49b359d5342db0c3266ca44e9b
805933 <805933@seq.org>
2021-09-14 11:13:31 -0700

    vector

--- lab 909 attraction/ball.js
+++ lab 909 attraction/ball.js
@@ -1,33 +1,40 @@
-function Ball (location, velocity){
+class Ball{
   constructor (x, y, dx, dy, radius, color){
-    this.x = x; //declare instance variables
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+    //this.x = x; //declare instance variables
+    //this.y = y;
+    this.loc = new JSVector(x,y); //new creates a new instance
+    //have a vector that already has an x and y
+    //this.dx = dx;
+    //this.dy = dy;
+    this.vel = new JSVector(dx,dy);
+    //have a vector that has velocity (use magnitude and direction)
+    //vector has magnitude and direction and so does velocity and acceleration
+    //so velocity can be represented as a vector (and so can acceleration)
     this.radius = radius;
     this.color = color;
     this.canvas = document.getElementById("cnv");
     this.context = this.canvas.getContext("2d");
   }
   update(){
-    this.x += this.dx;    // update x coordinate of location with x velocity
-    this.y += this.dy;
+    this.loc.add(this.vel)
+    this.loc.x += this.vel.x;    // update x coordinate of location with x velocity
+    this.loc.y += this.vel.y;
   }
   draw(){
     context.beginPath();    // clear old path
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.arc(this.loc.x, this.loc.y, this.radius, 0, 2 * Math.PI);
     context.strokeStyle = "black";  // color to fill
     context.fillStyle = this.color;     // color to stroke
     context.fill();     // render the fill
     context.stroke();
   }
   checkEdges(){
-      if(this.x<0 || this.x>canvas.width){ //instance variable to canvas height
-        this.dx *= -1
+      if(this.loc.x<0 || this.loc.x>canvas.width){ //instance variable to canvas height
+        this.vel.x *= -1
       }
-      if(this.y<0 || this.y>canvas.height){
-        this.dy *= -1
+      if(this.loc.y<0 || this.loc.y>canvas.height){
+        this.vel.y *= -1
       }
   }
   colorChange(){
