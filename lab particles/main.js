f4f90e7d9e08bb781f7d4d4f7b4cf1191a47e823
805933 <805933@seq.org>
2021-10-21 21:50:58 -0700

    completed lab with comments

--- lab particles/main.js
+++ lab particles/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", init);
 var canvas, context
-var particles = [];
+var particles = []; //fully aware of the fact that I'm using an array compared to something else
 function init(){
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
   canvas = document.getElementById("cnv");
@@ -10,31 +10,31 @@ function init(){
 }
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height);
-  createParticle();
+  createParticle(); //creates particle every frame
   window.requestAnimationFrame(animate);
   for (let i = 0; i<particles.length; i++){
     particles[i].run();
     if (particles[i].isDead() == true){
       particles.splice(i,1);
-      i--;
+      i--; //stops rendering if the particle has 'died'
     }
   }
 
 
 }
 function createParticle(){
-  for (let i = 0; i<1; i++){
+  for (let i = 0; i<1; i++){ //Setting the value to >1 creates multiple particles each frame
     let radius = 15;
-    let x = canvas.width/2 + Math.random()*100-50;
+    let x = canvas.width/2 + Math.random()*100-50; //middle of the canvas, with up to 50 pixels of variation in each direction
     let y = canvas.height/2 +Math.random()*100-50;
-    let dx = Math.random()*5 - 2.5;
-    let dy = Math.random()*7.5 - 3.75;
+    let dx = Math.random()*5 - 2.5; //initial velocity of x, can be anywhere from -2.5 to 2.5.
+    let dy = Math.random()*7.5 - 3.75; //can be anywhere from -3.75 to 3.75
     let r = Math.floor(Math.random()*255);
     let g = Math.floor(Math.random()*255);
     let b = Math.floor(Math.random()*255);
-    let a = 1;
+    let a = 1; //gives particle random color and sets to 100% opacity
     let clr = "rgba("+r+","+g+","+b+","+a+")"
-    let life = Math.floor(Math.random()*90)+90;
+    let life = Math.floor(Math.random()*90)+90; //particle can last from 1.5 to 3 seconds
     particles.push(new Particle(x, y, dx, dy, radius, clr, life, r, g, b))
     }
   }
